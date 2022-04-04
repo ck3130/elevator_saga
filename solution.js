c073fe8bedfe4ecfe18e90ffa19e6e976e60826e
conrad <conradkersten@gmail.com>
2022-04-04 00:15:51 -0500

    challenge 2 refactor

--- solution.js
+++ solution.js
@@ -16,40 +16,23 @@
         elevator.on("floor_button_pressed", function(floorNum) {
             elevator.goToFloor(floorNum);
         })
-        
-        function doWork(queue) {
-            // Find the floor where the most work can be done. 
-            if (queue.length == 0) return undefined
-            let valuesPresent = queue.values();
-            // Set as first value in queue. Return this is all values counts are same
-            let floor = queue[0];
-            let highestCount = 0;
-            for (let value of valuesPresent) {
-                let tempCount = queue.filter(x => x == value).length;
-                if (highestCount == 0) {
-                    highestCount = tempCount
-                } else {
-                    if (tempCount > highestCount) {
-                        highestCount = tempCount;
-                        floor = value;
-                    };
-                };
-            };
-        }
-        
 
         // Filter elevator destination queue to avoid unproductive trips
         elevator.on("stopped_at_floor", function(floorNum) {
             elevator.destinationQueue = elevator.destinationQueue.filter(x => x != floorNum);
-            let bestFloor = doWork(elevator.destinationQueue);
-            elevator.destinationQueue = bestFloor ? [bestFloor, ...elevator.destinationQueue] : elevator.destinationQueue;
             elevator.checkDestinationQueue();
+        })
+        
+        // Check to see if a floor being passed should be stopped at
+        elevator.on("passing_floor", function(floorNum, direction){
             
+            if (elevator.destinationQueue.some(x => x == floorNum)) elevator.destinationQueue.unshift(floorNum);
+            elevator.checkDestinationQueue();
         })
 
 
     },
         update: function(dt, elevators, floors) {
-            // We normally don't need to do anything here
+            
         }
 }
