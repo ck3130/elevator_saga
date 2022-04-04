fd4d42692aa23762bb70065f4e27df6ac9bc440b
conrad <conradkersten@gmail.com>
2022-04-04 13:54:14 -0500

    challenge 5

--- solution.js
+++ solution.js
@@ -1,8 +1,8 @@
 {
-    init: function(elevators, floors) 
+    init: function(elevators, floors) {
         
         function floorAssign(elevators){
-            // Function to see which elevator has the least number of queued destinations. Return that elevator.
+            // Function to see which elevator has the least number of queued destinations. 
             let assignedElevator = elevators[0];
             let taskCount = 0;
             for (let elevator of elevators) {
@@ -19,14 +19,27 @@
             };
             return assignedElevator;
         }
+        
+        function clearFloor(floor, elevators) {
+            // This function clears floor events that were added to elevators if a different elevator was recently at that floor
+            // It differentiates between where the elevator as called from a floor vs a passenger on board
+            // If a passenger needs to travel to that floor, it will still make that trip
+            for (let elevator of elevators) {
+                let pressedFloors = elevator.getPressedFloors();
+                if (elevator.destinationQueue.some(x => x == floor) && !pressedFloors.some(x => x == floor)) {
+                    elevator.destinationQueue = elevator.destinationQueue.filter(x => x != floor);
+                    elevator.checkDestinationQueue();
+                };
+            };
+        }
 
         // Add event listeners to each floor
         for (let floor of floors) {
             if (floor.floorNum() < floors.length) floor.on("up_button_pressed", function(){
-                floorAssign(elevators).goToFloor(floor.floorNum());
+                floorAssign(elevators).goToFloor(floor.floorNum())
             });
             if (floor.floorNum() > 0) floor.on("down_button_pressed", function(){
-                floorAssign(elevators).goToFloor(floor.floorNum());
+                floorAssign(elevators).goToFloor(floor.floorNum())
             });
         }
 
@@ -42,18 +55,21 @@
             elevator.on("stopped_at_floor", function(floorNum) {
                 elevator.destinationQueue = elevator.destinationQueue.filter(x => x != floorNum);
                 elevator.checkDestinationQueue();
+                clearFloor(floorNum, elevators);                
             })
        }
 
         // Check to see if a floor being passed should be stopped at
         for (let elevator of elevators) {
             elevator.on("passing_floor", function(floorNum, direction){
-                if (elevator.destinationQueue.some(x => x == floorNum)) elevator.destinationQueue.unshift(floorNum);
+                if (
+                    (elevator.destinationQueue.some(x => x == floorNum) && elevator.loadFactor() < .75) 
+                    || elevator.getPressedFloors().some(x => x == floorNum)
+                ) elevator.destinationQueue.unshift(floorNum);
                 elevator.checkDestinationQueue();
             })
         }
     },
         update: function(dt, elevators, floors) {
-
         }
 }
